5edc447eb23566453bfc38a606ea3fab9060aa95
braoutch <antoinelassagne@gmail.com>
2015-11-30 02:49:34 +0100

    Initial Release - v0.1
    - Working thermometer and alert buzzer
    - Screen initialization
    - Frizing started

---
+++ Aquaduino.ino
@@ -0,0 +1,107 @@
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h> // Inclusion de la librairie OneWire
+#include <Wire.h> 
+
+
+
+ 
+#define DS18B20 0x28     // Adresse 1-Wire du DS18B20
+#define BROCHE_ONEWIRE 7 // Broche utilisée pour le bus 1-Wire
+
+int buzzerPin = 4;
+int ledPin = 8;
+bool buzzerOn = false ;
+
+//LiquidCrystal_I2C lcd(0x20, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Addr, En, Rw, Rs, d4, d5, d6, d7, backlighpin, polarity
+LiquidCrystal_I2C lcd(0x3F,20,4); 
+//LiquidCrystal_I2C lcd(0x37,20,4); 
+
+OneWire ds(BROCHE_ONEWIRE); // Création de l'objet OneWire ds
+
+// Fonction récupérant la température depuis le DS18B20
+// Retourne true si tout va bien, ou false en cas d'erreur
+boolean getTemperature(float *temp)
+{
+  byte data[9], addr[8];
+  // data : Données lues depuis le scratchpad
+  // addr : adresse du module 1-Wire détecté
+ 
+  if (!ds.search(addr)) { // Recherche un module 1-Wire
+    ds.reset_search();    // Réinitialise la recherche de module
+    return false;         // Retourne une erreur
+}
+
+   if (OneWire::crc8(addr, 7) != addr[7]) // Vérifie que l'adresse a été correctement reçue
+    return false;                        // Si le message est corrompu on retourne une erreur
+ 
+  if (addr[0] != DS18B20) // Vérifie qu'il s'agit bien d'un DS18B20
+    return false;         // Si ce n'est pas le cas on retourne une erreur
+ 
+  ds.reset();             // On reset le bus 1-Wire
+  ds.select(addr);        // On sélectionne le DS18B20
+   
+  ds.write(0x44, 1);      // On lance une prise de mesure de température
+  delay(800);             // Et on attend la fin de la mesure
+   
+  ds.reset();             // On reset le bus 1-Wire
+  ds.select(addr);        // On sélectionne le DS18B20
+  ds.write(0xBE);         // On envoie une demande de lecture du scratchpad
+ 
+  for (byte i = 0; i < 9; i++) // On lit le scratchpad
+    data[i] = ds.read();       // Et on stock les octets reçus
+   
+  // Calcul de la température en degré Celsius
+  *temp = ((data[1] << 8) | data[0]) * 0.0625; 
+   
+  // Pas d'erreur
+  return true;
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin,HIGH);
+
+  //Display init
+  lcd.init(); 
+  lcd.backlight();
+  lcd.setCursor(0, 0);  // (Colonne,ligne)
+}
+
+// the loop routine runs over and over again forever:
+void loop() 
+{
+   float temp;
+  // Lit la température ambiante à ~1Hz
+  if(getTemperature(&temp)) {
+     
+    // Affiche la température
+    Serial.print("Temperature : ");
+    Serial.print(temp);
+    Serial.write(176); // caractère °
+    Serial.write('C');
+    Serial.println();
+
+      if(temp != 0 && temp > 30)
+  {
+    digitalWrite(buzzerPin, HIGH);
+    Serial.println("TEMPERATURE TOO HIGH");
+  }
+
+  if(temp != 0 && temp < 29)
+  {
+  digitalWrite(buzzerPin, LOW);
+  Serial.println("TEMPERATURE GOOD");
+  }
+  
+  }
+
+
+}
+
+
+
